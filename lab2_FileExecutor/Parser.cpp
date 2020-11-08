cd2af6b4bfab89f94f37b1716ac98856223f41b7
Danil <d.sinitsyn@g.nsu.ru>
2020-11-08 22:59:23 +0700

    small changes in files for Parser

--- lab2_FileExecutor/Parser.cpp
+++ lab2_FileExecutor/Parser.cpp
@@ -1,1 +1,19 @@
 #include "Parser.h"
+
+Parser::Parser(std::string filename) {
+    this->filename = std::move(filename);
+}
+
+void Parser::parsing() {
+    std::ifstream stream;
+    stream.open(filename);
+    if(!stream.is_open()){
+        throw "Bad name of input file";
+    }
+    std::string str;
+    stream >> str;
+    if(str != "desc"){
+        throw "Bad format";
+    }
+    std::regex rx("([0-9]) = (grep|sort|replace|readfile|writefile) *((\\w+(.| )\\w*)|)");
+}
--- lab2_FileExecutor/Parser.h
+++ lab2_FileExecutor/Parser.h
@@ -2,9 +2,18 @@
 #include<regex>
 #include<string>
 #include<vector>
+#include<map>
+#include<fstream>
 
 class Parser {
+    explicit Parser(std::string filename);
+    void parsing ();
 
+private:
+    std::string filename;
+    std::vector<int> order;
+    std::map<int, std::vector<std::string>> workers; // in x.second[0] is name of worker
+                                                     // in x.second[1-n] is args
 };
 
 
