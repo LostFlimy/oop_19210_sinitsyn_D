438da792aaeb5dc71f47e2b8d4bc023eda11dfe5
Danil <d.sinitsyn@g.nsu.ru>
2020-11-08 15:05:23 +0700

    Add Workers

--- lab2_FileExecutor/Workers.h
+++ lab2_FileExecutor/Workers.h
@@ -3,8 +3,33 @@
 #include<vector>
 #include<fstream>
 
-class Workers {
+class Worker {
+public:
+    enum class ReturnType {
+        NONE, TEXT
+    };
+    class WorkerResult {
+    private:
+        std::vector<std::string> text;
+    public:
+        WorkerResult();
+        explicit WorkerResult(std::vector<std::string>& text) : text(text){}
+        std::vector<std::string> getText();
+    };
+    virtual WorkerResult& operation(WorkerResult& enter) = 0;
+    ReturnType getInput() const;
+    ReturnType getOutput() const;
+protected:
+    ReturnType inputType;
+    ReturnType outputType;
 
+    explicit Worker(ReturnType input, ReturnType output) : inputType(input), outputType(output){}
+};
+
+class readWorker : public Worker
+{
+public:
+    WorkerResult& operation(WorkerResult& enter) override;
 };
 
 
